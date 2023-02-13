e1fe6b274db1945229758c2cdea22e9992018f95
Aleksandr <azelen04@mail.ru>
2023-02-13 16:14:39 +0300

    Made task 1.2

--- 1_lab/pass_by_reference.cpp
+++ 1_lab/pass_by_reference.cpp
@@ -2,6 +2,7 @@
 #define CLION_PROGRAMMING_PASS_BY_REFERENCE_C
 #include "pass_by_reference.h"
 #include <iostream>
+#include <cmath>
 
 void swap_values_ref(int &fir_var, int &sec_var) {
     int gap = fir_var;
@@ -15,13 +16,31 @@ void swap_values_pntr(int *fir_var, int *sec_var) {
     *sec_var = temp;
 }
 
-void test_swap_values(int exp_fir_var, int exp_sec_var, int fact_fir_var, int fact_sec_var) {
+void test_swap_values(int &exp_fir_var, int &exp_sec_var, int &fact_fir_var, int &fact_sec_var) {
     if (exp_fir_var == fact_fir_var && exp_sec_var == fact_sec_var) {
         std::cout << "Переменные поменялись местами!!!" << "\n";
         std::cout << exp_fir_var << " = " << fact_fir_var << ", " << exp_sec_var << " = " << fact_sec_var << "\n";
     } else {
         std::cout << "Переменные не поменялись местами;(" << "\n";
         std::cout << exp_fir_var << " != " << fact_fir_var << ", " << exp_sec_var << " != " << fact_sec_var << "\n";
+    }
+}
+
+void disc_func_part_ref(float &var) {
+    var = floor(var);
+}
+
+void disc_func_part_pntr(float *var) {
+    *var = floor(*var);
+}
+
+void test_disc_func_part(float &exp_var, float &fact_var) {
+    if (exp_var == fact_var) {
+        std::cout << "Переменные совпадают!!!" << "\n";
+        std::cout << exp_var << " = " << fact_var << "\n";
+    } else {
+        std::cout << "Переменные не совпадают" << "\n";
+        std::cout << exp_var << " != " << fact_var << "\n";
     }
 }
 
@@ -34,6 +53,15 @@ void lab_1() {
     int *pntr_a = &a, *pntr_b = &b;
     swap_values_pntr(pntr_a, pntr_b);
     test_swap_values(cp_a, cp_b, a, b);
+    float dfp;
+    std::cin >> dfp;
+    float dfp_cp = floor(dfp);
+    disc_func_part_ref(dfp);
+    test_disc_func_part(dfp, dfp_cp);
+    std::cin >> dfp;
+    float *pntr_dfp = &dfp;
+    disc_func_part_pntr(pntr_dfp);
+    test_disc_func_part(dfp, dfp_cp);
 };
 
 #endif //CLION_PROGRAMMING_PASS_BY_REFERENCE_C
--- 1_lab/pass_by_reference.h
+++ 1_lab/pass_by_reference.h
@@ -5,7 +5,13 @@ void swap_values_ref(int &fir_var, int &sec_var);
 
 void swap_values_pntr(int *fir_var, int *sec_var);
 
-void test_swap_values(int exp_fir_var, int exp_sec_var, int fact_fir_var, int fact_sec_var);
+void test_swap_values(int &exp_fir_var, int &exp_sec_var, int &fact_fir_var, int &fact_sec_var);
+
+void disc_func_part_ref(float &var);
+
+void disc_func_part_pntr(float *var);
+
+void test_disc_func_part(float &exp_var, float &fact_var);
 
 void lab_1();
 
