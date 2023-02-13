3890c0d96c1e4cf058dbfee22fbf155d20f03cc1
Aleksandr <azelen04@mail.ru>
2023-02-13 12:53:11 +0300

    Added functions to header

--- 1_lab/pass_by_reference.h
+++ 1_lab/pass_by_reference.h
@@ -3,7 +3,9 @@
 
 void swap_values_ref(int &fir_var, int &sec_var);
 
-void swap_values_pntr(int *fir_var, int sec_var);
+void swap_values_pntr(int *fir_var, int *sec_var);
+
+void test_swap_values(int exp_fir_var, int exp_sec_var, int fact_fir_var, int fact_sec_var);
 
 void lab_1();
 
