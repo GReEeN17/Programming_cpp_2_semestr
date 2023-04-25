8e667f4f2110a34110cbb2463c190ab0ef04bc47
Aleksandr <azelen04@mail.ru>
2023-04-25 16:41:39 +0300

    Made first variant of Circular Buffer, not sure about setting capacity

--- 7_lab/CircularBuffer.h
+++ 7_lab/CircularBuffer.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <algorithm>
 
+using namespace std;
+
 template<class T>
 class CircularBuffer{
 private:
@@ -84,7 +86,91 @@ public:
         }
     };
 
+    explicit CircularBuffer(int capacity) {
+        this->size = 0;
+        this->capacity = capacity;
+        this->buffer = new T[capacity];
+        this->bufferFirst = buffer[0];
+        this->bufferLast = buffer[capacity - 1];
+        this->currentFirst = buffer[0];
+        this->currentLast = buffer[0];
+    }
 
+    int getSize() {
+        return this->size;
+    }
+
+    int getCapacity() {
+        return this->capacity;
+    }
+
+    Iterator start() {
+        return Iterator(buffer);
+    }
+
+    Iterator end() {
+        return Iterator(buffer + capacity - 1);
+    }
+
+    void pushFront(T &value) {
+        *currentFirst = value;
+        if (currentFirst == bufferFirst) {
+            currentFirst = bufferLast;
+        } else {
+            currentFirst--;
+        }
+    }
+
+    void pushBack(T &value) {
+        *currentLast = value;
+        if (currentLast == bufferLast) {
+            currentLast = bufferFirst;
+        } else {
+            currentLast++;
+        }
+    }
+
+    void popFront() {
+        *currentFirst = 0;
+        if (currentFirst == bufferLast) {
+            currentFirst = bufferFirst;
+        } else {
+            currentFirst++;
+        }
+    }
+
+    void popBack() {
+        *currentLast = 0;
+        if (currentLast == bufferFirst) {
+            currentLast = bufferLast;
+        } else {
+            currentLast--;
+        }
+    }
+
+    void setCapacity(int capacity) {
+        if (this->capacity >= capacity) {
+            cout << "Set capacity more than current" << "\n";
+            return;
+        }
+        T* gapBuffer = new T[capacity];
+        for (int i = 0; i < this->capacity; i++) {
+            gapBuffer[i] = *buffer[i];
+        }
+        buffer = gapBuffer;
+        this->capacity = capacity;
+    }
+
+    T operator [] (int index) const {
+        return buffer[index % capacity];
+    }
+
+    void print() {
+        for (T i = &buffer[0]; i != &buffer[capacity - 1]; i++) {
+            cout << *i << " ";
+        }
+        cout << "\n";
+    }
 };
 
 #endif //CLION_PROGRAMMING_CIRCULARBUFFER_H
